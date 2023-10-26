e586a54ebd906fd9260476905f552db77d9c23c8
WilliamCary <williamandrewcary@gmail.com>
2023-10-26 15:20:52 -0400

    chore: Initial setup

---
+++ pages/About.tsx
@@ -0,0 +1,11 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <Navbar />
+      <h2>About</h2>
+    </div>
+  )
+}
