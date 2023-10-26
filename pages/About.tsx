d4e970efd31e23a3e932275923af6e46ec388d9b
WilliamCary <williamandrewcary@gmail.com>
2023-10-26 16:18:10 -0400

    test: testing codeium auto generation

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,13 +1,19 @@
 import Navbar from '@/components/Navbar'
 import React from 'react'
+import { useState } from 'react'
 
 export default function About() {
+  //turn this code into a counter function with plus and minus buttons to change state
+  const [counter, setCounter] = useState(0)
   return (
     <div>
       <Navbar />
       <h2>About</h2>
       <p>This is the about page</p>
       <p>And this is a test change</p>
+      <h2>{counter}</h2>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter(counter - 1)}>-</button>
     </div>
   )
 }
