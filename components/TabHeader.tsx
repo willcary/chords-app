e90e53da4fb6b0c9022c51cce35e3a3e12e9fd35
WilliamCary <williamandrewcary@gmail.com>
2023-11-01 11:52:37 -0400

    feat: Add TabHeader component

---
+++ components/TabHeader.tsx
@@ -0,0 +1,19 @@
+import TabHeaderO from './symbols/TabHeaderO'
+import TabHeaderX from './symbols/TabHeaderX'
+
+export default function TabHeader() {
+  return (
+    <>
+      <h2>Dsus2</h2>
+      <div className='tab-header'>
+        <TabHeaderX />
+        <TabHeaderX />
+        <TabHeaderO />
+        <TabHeaderO />
+        <TabHeaderO />
+        <TabHeaderO />
+      </div>
+      <p className=''>5</p>
+    </>
+  )
+}
