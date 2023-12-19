cde0a2499766753e1b232c9e6bb8c62e62bc26c4
willcary <w.a.c.ary@gmail.com>
2023-12-19 11:37:01 -0500

    feat: add getStaticProps to index.tsx
    Still buggy.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 import Navbar from '@/components/Navbar'
 import { Inter } from 'next/font/google'
 import Tabs from '@/components/Tabs'
@@ -6,7 +7,25 @@ import exampleChord from '@/assets/exampleChord'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+interface AllChords {
+  main: {
+    strings: number
+    fretsOnChord: number
+    name: string
+    numberOfChords: number
+  }
+  tunings: {
+    standard: string[]
+  }
+  keys: string[]
+  suffixes: string[]
+  chords: {}
+}
+
+export default function Home({
+  allChords,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log(allChords)
   return (
     <>
       <Head>
@@ -21,12 +40,28 @@ export default function Home() {
       <Navbar />
       <main>
         <h1>Guitar Chords</h1>
+        <div>
+          <div>Key:</div>
+          <div>Suffix:</div>
+        </div>
         <Tabs
           tone={exampleChord.key}
           suffix={exampleChord.suffix}
           positions={exampleChord.positions}
         />
+        <div>{allChords.main.name}</div>
       </main>
     </>
   )
 }
+
+export const getStaticProps = (async () => {
+  const res = await fetch('http://localhost:3000/api/C')
+  const allChords: AllChords = await res.json()
+
+  return {
+    props: {
+      allChords,
+    },
+  }
+}) satisfies GetStaticProps<{ allChords: AllChords }>
